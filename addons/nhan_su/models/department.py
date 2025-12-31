9fcd0e9fded1875509c744ac671a9137b70a9937
vitvit <tavietanh1012004@gmail.com>
2025-12-31 15:16:14 +0700

    Fix: add models.xml and update manifest for nhan_su

--- addons/nhan_su/models/department.py
+++ addons/nhan_su/models/department.py
@@ -5,5 +5,17 @@ class Department(models.Model):
     _description = 'Phòng ban'
 
     name = fields.Char('Tên phòng ban', required=True)
-    code = fields.Char('Mã phòng ban')
+    code = fields.Char('Mã phòng ban', readonly=True, copy=False, default=lambda self: self._generate_code())
+
+    @api.model
+    def create(self, vals):
+        if not vals.get('code'):
+            vals['code'] = self._generate_code()
+        return super().create(vals)
+
+    @api.model
+    def _generate_code(self):
+        last = self.search([], order='id desc', limit=1)
+        next_id = (last.id or 0) + 1
+        return 'PB%03d' % next_id
     employee_ids = fields.One2many('nhan_su.employee', 'department_id', string='Nhân viên')
--- addons/nhan_su/models/position.py
+++ addons/nhan_su/models/position.py
@@ -5,5 +5,17 @@ class Position(models.Model):
     _description = 'Chức vụ'
 
     name = fields.Char('Tên chức vụ', required=True)
-    code = fields.Char('Mã chức vụ')
+    code = fields.Char('Mã chức vụ', readonly=True, copy=False, default=lambda self: self._generate_code())
+
+    @api.model
+    def create(self, vals):
+        if not vals.get('code'):
+            vals['code'] = self._generate_code()
+        return super().create(vals)
+
+    @api.model
+    def _generate_code(self):
+        last = self.search([], order='id desc', limit=1)
+        next_id = (last.id or 0) + 1
+        return 'CV%03d' % next_id
     employee_ids = fields.One2many('nhan_su.employee', 'position_id', string='Nhân viên')
