6de0dc4481a131d70102a68bac10c92bd7d37497
vitvit <tavietanh1012004@gmail.com>
2026-01-14 15:19:10 +0700

    UD

--- addons/customer_document_management/models/document_file.py
+++ addons/customer_document_management/models/document_file.py
@@ -1,19 +1,29 @@
 from odoo import models, fields
 
 class DocumentFile(models.Model):
+    _name = 'customer.document.file'
+    _description = 'Tệp đính kèm văn bản khách hàng'
 
-        def action_view_file(self):
-            """
-            Trả về action để xem file trực tiếp trên trình duyệt (inline).
-            """
-            self.ensure_one()
-            if not self.file:
-                return
-            return {
-                'type': 'ir.actions.act_url',
-                'url': f'/api/document_file/view/{self.id}',
-                'target': 'self',
-            }
+    name = fields.Char(string='Tên file')
+    file = fields.Binary(string='File', required=True)
+    document_id = fields.Many2one('customer.document.document', string='Văn bản', required=True, ondelete='cascade')
+    upload_user_id = fields.Many2one('res.users', string='Người upload', default=lambda self: self.env.uid)
+    upload_date = fields.Datetime(string='Ngày upload', default=fields.Datetime.now)
+
+    download_url = fields.Char(string='Tải về', compute='_compute_download_url', store=False)
+
+    def action_view_file(self):
+        """
+        Trả về action để xem file trực tiếp trên trình duyệt (inline).
+        """
+        self.ensure_one()
+        if not self.file:
+            return
+        return {
+            'type': 'ir.actions.act_url',
+            'url': f'/api/document_file/view/{self.id}',
+            'target': 'self',
+        }
     _name = 'customer.document.file'
     _description = 'Tệp đính kèm văn bản khách hàng'
 
