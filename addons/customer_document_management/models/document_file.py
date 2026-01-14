8f317911ae886c003a8aca6180ac7763fea0f061
vitvit <tavietanh1012004@gmail.com>
2026-01-14 13:40:58 +0700

    UD

--- addons/customer_document_management/models/document_file.py
+++ addons/customer_document_management/models/document_file.py
@@ -10,6 +10,25 @@ class DocumentFile(models.Model):
     upload_user_id = fields.Many2one('res.users', string='Người upload', default=lambda self: self.env.uid)
     upload_date = fields.Datetime(string='Ngày upload', default=fields.Datetime.now)
 
+    download_url = fields.Char(string='Tải về', compute='_compute_download_url', store=False)
+
+    def _compute_download_url(self):
+        for rec in self:
+            if rec.id:
+                rec.download_url = f'/web/content/customer.document.file/{rec.id}/file/{rec.name or "file"}?download=true'
+            else:
+                rec.download_url = ''
+
+    def download_file(self):
+        self.ensure_one()
+        if not self.file:
+            return
+        return {
+            'type': 'ir.actions.act_url',
+            'url': self.download_url,
+            'target': 'self',
+        }
+
     def download_file_action(self):
         """
         Trả về action để tải file về từ giao diện Odoo.
