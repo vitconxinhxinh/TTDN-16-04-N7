606668346c13b5e8f978a8b9b1b3ccb45fecc599
vitvit <tavietanh1012004@gmail.com>
2026-01-28 15:47:41 +0700

    UD

--- addons/customer_document_management/models/document_file.py
+++ addons/customer_document_management/models/document_file.py
@@ -1,6 +1,5 @@
 from odoo import models, fields, api
 
-
 class DocumentFile(models.Model):
     _name = 'customer.document.file'
     _description = 'Tệp đính kèm văn bản khách hàng'
@@ -34,25 +33,7 @@ class DocumentFile(models.Model):
                 'sticky': False,
             }
         }
-from odoo import models, fields, api
 
-
-class DocumentFile(models.Model):
-    _name = 'customer.document.file'
-    _description = 'Tệp đính kèm văn bản khách hàng'
-
-    name = fields.Char(string='Tên file')
-    file = fields.Binary(string='File', required=True)
-    document_id = fields.Many2one('customer.document.document', string='Văn bản', required=True, ondelete='cascade')
-    upload_user_id = fields.Many2one('res.users', string='Người upload', default=lambda self: self.env.uid)
-    upload_date = fields.Datetime(string='Ngày upload', default=fields.Datetime.now)
-    download_url = fields.Char(string='Tải về', compute='_compute_download_url', store=False)
-    suggested_document_type = fields.Selection([
-        ('contract', 'Hợp đồng'),
-        ('quotation', 'Báo giá'),
-        ('legal', 'Tài liệu pháp lý'),
-        ('other', 'Khác')
-    ], string='Nhãn gợi ý', readonly=True, copy=False)
     @classmethod
     def _suggest_label_from_ai(cls, text):
         import requests
